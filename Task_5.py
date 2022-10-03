80679826ad8d8679e404865821925ea9cc95889b
Gaiah <gaiah.a.akopian@gmail.com>
2022-10-03 22:34:31 +0400

    Task 5 added

--- Task_5.py
+++ Task_5.py
@@ -1,11 +1,12 @@
 ##.5 Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 #- для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] [Негафибоначчи]
 import math
+import time
 
 user_n = abs(int(input("Input Fibonacci N: ")))
 list_negative = []
 list_positive = []
-
+start_time = time.time()
 def fibonacci(n):
     if n == -1:
         return 1
@@ -18,11 +19,11 @@ def fibonacci(n):
 
 
 for i in range(0, user_n + 1):
-    list_negative.append(fibonacci(i) * ((-1)**(i+1)))
-for k in list_negative[::-1]:
-    print(k, end = "    ")
+    list_negative.insert(0, fibonacci(i) * ((-1)**(i+1)))
 
 for i in range(1, user_n + 1):
-    list_positive.append(fibonacci(i))
-for j in list_positive:
-    print(j, end = "    ")
+    list_negative.append(fibonacci(i))
+for i in list_negative:
+     print(i, end = "    ")
+end_time = time.time()
+print(end_time - start_time)
